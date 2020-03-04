631352286cac1eef25fe206da184cb1f05cdc05b
arwildo <arwildo@gmail.com>
2020-03-05 00:59:20 +0800

    add repeat question 3 times, reset if !correct

--- question.py
+++ question.py
@@ -1,16 +1,38 @@
 import random
 
-print("Question: ")
 
-numone = random.randint(0, 100)
-numtwo = random.randint(0, 11)
-result = numone * numtwo
+def quickMath():
+    print("No. {}".format(correct_ct))
+    numone = random.randint(0, 100)
+    numtwo = random.randint(0, 11)
+    result = numone * numtwo
 
-answer = int(input("{} x {} = ".format(numone, numtwo)))
-print("{} x {} = {}".format(numone, numtwo, result))
-print()
+    answer = int(input("Question: {} x {} = ".format(numone, numtwo)))
+    print("Result: {} x {} = {}".format(numone, numtwo, result))
 
-if answer == result:
-    print("Correct")
-else:
-    print("Incorrect!")
+    if answer == result:
+        return True
+    else:
+        return False
+
+
+
+if __name__ == "__main__":
+    correct_ct = 1
+
+    while correct_ct < 4:
+        correct = quickMath()
+        print("---------------------------")
+
+        if correct:
+            print("Correct")
+            print("{}/3".format(correct_ct))
+            correct_ct += 1
+        else:
+            print("Incorrect!")
+            print("Reset correct counter")
+            correct_ct = 0
+
+        print()
+        print()
+        print()
